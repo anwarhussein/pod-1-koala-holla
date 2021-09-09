4057201bc9bdf2294f56085173b56413ab774ddf
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-09-09 15:44:15 -0500

    NEW database fixed.

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE TABLE "koalas" (
     "id" SERIAL PRIMARY KEY,
     "name" varchar(80) not null,
     "gender" varchar(1),
-    "age" int(2),
+    "age" int,
     "ready_to_transfer" boolean,
     "notes" varchar(80)
 );
@@ -10,9 +10,9 @@ CREATE TABLE "koalas" (
 INSERT INTO "koalas"
 ("name", "gender","age","ready_to_transfer", "notes")
 VALUES
-('Scotty', 'M', '4', 'Y', 'Born in Guatemala')
-('Jean', 'F', '5', 'Y', 'Allergic to lots of lava')
-('Ororo ','F' ,'7' ,'N' ,'Loves listening to Paula (Abdul)')
-('Logan','M','15', 'N', 'Loves the Sauna')
-('Charlie','M','9', 'Y', 'Favorite band is Nirvana')
-('Betsy','F','4', 'Y', 'Has a pet cat')
+('Scotty', 'M', '4', 'Y', 'Born in Guatemala'),
+('Jean', 'F', '5', 'Y', 'Allergic to lots of lava'),
+('Ororo ','F' ,'7' ,'N' ,'Loves listening to Paula (Abdul)'),
+('Logan','M','15', 'N', 'Loves the Sauna'),
+('Charlie','M','9', 'Y', 'Favorite band is Nirvana'),
+('Betsy','F','4', 'Y', 'Has a pet cat');
