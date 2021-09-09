02c93c0c6ba5baf0a1ace7dd2645dca28e85ecd8
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-09-09 16:11:37 -0500

    added function that takes in inputs and posts them

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,38 +3,45 @@ console.log( 'js' );
 $( document ).ready( function(){
   console.log( 'JQ' );
   // Establish Click Listeners
-  setupClickListeners()
+  clickListeners();
   // load existing koalas on page load
   getKoalas();
 
 }); // end doc ready
 
-function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+//listens for clicks on any buttons/inputs
+function clickListeners() {
+  $( '#addButton' ).on( 'click', postKoala);
 }
+
+//adds the new koala from the inputs when #addButton is clicked
+function postKoala() {
+  console.log( 'in addButton on click' );
+  let koalaToSend = {
+    name: $('#nameIn').val(),
+    age: $('#ageIn').val(),
+    gender: $('#genderIn').val(),
+    readyForTransfer: ($('#readyForTransferIn').val().toLowerCase() === 'y'), //returns true or false if input is 'y' or 'n' respectively
+    notes: $('#notesIn').val(),
+  };
+  console.log(koalaToSend)
+  // call saveKoala with the new obejct
+  $.ajax({
+    type: 'POST',
+    url: '/koalas',
+    data: koalaToSend,
+    }).then(function(response) {
+      console.log('Response from server.', response);
+      getKoalas();
+    }).catch(function(error) {
+      console.log('Error in POST', error)
+      alert('Unable to add koala.');
+    });
+}; 
+
 
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
   
 } // end getKoalas
-
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
- 
-}
