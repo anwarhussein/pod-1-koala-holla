fe73e95bf5fd900314de8f40a1c38ce37adb87b9
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-09-09 21:43:51 -0500

    route to mark for transfer (PUT) koala

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,16 +1,37 @@
+const { Router, query } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
 
 
-// GET
+// GET anwar
 
 
-// POST
+// POST franck
 
 
-// PUT
+// PUT phaydara
+/**
+ * @api {put} /:id ready to transer true
+ * @apiDescription set the ready to transfer to true
+ * based on id that matches.
+ * 
+ * @apiParam {number} id, the id of the koala to transfer
+ */
+router.put('/:id', (req, res) => {
+    console.log(req.params);
+    const koalaId = req.params.id;
+    const queryKoala = `UPDATE FROM "koalas" 
+                            SET "ready_to_transfer" = TRUE 
+                            WHERE "ready_to_transfer" = FALSE;`;
+    pool.query(queryKoala, [koalaId]).then((result) => {
+        res,sendStatus(200);
+    }).catch((error) => {
+        console.log('error PUT /koalas', error);
+        res.sendStatus(500);  
+    });   
+});
 
 
 // DELETE
