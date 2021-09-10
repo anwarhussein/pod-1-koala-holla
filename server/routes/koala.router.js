48f1b0c96702de4f1c9929fcdb19b45567f36768
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-09-10 10:21:28 -0500

    resolved merge conflict

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -22,34 +22,6 @@ pool.on('error', (error) => {
 
 
 
-// GET anwar
-
-
-// POST franck
-
-
-// PUT phaydara
-/**
- * @api {put} /:id ready to transer true
- * @apiDescription set the ready to transfer to true
- * based on id that matches.
- * 
- * @apiParam {number} id, the id of the koala to transfer
- */
-koalaRouter.put('/:id', (req, res) => {
-    console.log(req.params);
-    const koalaId = req.params.id;
-    const queryKoala = `UPDATE FROM "koalas" 
-                            SET "ready_to_transfer" = TRUE 
-                            WHERE "ready_to_transfer" = FALSE;`;
-    pool.query(queryKoala, [koalaId]).then((result) => {
-        res,sendStatus(200);
-    }).catch((error) => {
-        console.log('error PUT /koalas', error);
-        res.sendStatus(500);  
-    });   
-});
-=======
 // GET
 
 
@@ -73,7 +45,27 @@ router.post('/', (req, res) => {
     });
 });
 
-// PUT
+// PUT phaydara
+/**
+ * @api {put} /:id ready to transer true
+ * @apiDescription set the ready to transfer to true
+ * based on id that matches.
+ * 
+ * @apiParam {number} id, the id of the koala to transfer
+ */
+ koalaRouter.put('/:id', (req, res) => {
+    console.log(req.params);
+    const koalaId = req.params.id;
+    const queryKoala = `UPDATE FROM "koalas" 
+                            SET "ready_to_transfer" = TRUE 
+                            WHERE "ready_to_transfer" = FALSE;`;
+    pool.query(queryKoala, [koalaId]).then((result) => {
+        res,sendStatus(200);
+    }).catch((error) => {
+        console.log('error PUT /koalas', error);
+        res.sendStatus(500);  
+    });   
+});
 
 
 // DELETE
